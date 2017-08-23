a308329c6ee89d795977a86c8d760b6786bcfab8
Hervé <herve.jouin@gmail.com>
2017-08-23 21:01:50 +0200

    new

---
+++ sources/src/org/archicontribs/specialization/uiProvider/ApplicationInteractionUIProvider.java
@@ -0,0 +1,54 @@
+/**
+ * This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ */
+package org.archicontribs.specialization.uiProvider;
+
+import org.archicontribs.specialization.SpecializationPlugin;
+import org.archicontribs.specialization.figure.InteractionFigure;
+import org.eclipse.gef.EditPart;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
+
+import com.archimatetool.editor.diagram.editparts.ArchimateElementEditPart;
+
+
+/**
+ * Node UI Provider
+ * 
+ * @author Herve Jouin
+ */
+public class ApplicationInteractionUIProvider extends com.archimatetool.editor.ui.factory.elements.ApplicationInteractionUIProvider {
+    @Override
+    public EditPart createEditPart() {
+            // we override the standard method because we want our NodeFigure class to be called
+        return new ArchimateElementEditPart(InteractionFigure.class);
+    }
+    
+    /**
+     * Gets the icon image from the component's properties. If not found, return the default one.
+     */
+    @Override
+    public Image getImage() {
+        String iconName = SpecializationPlugin.getIconName(instance, true);
+        
+        if ( iconName == null )
+            return super.getImage();
+
+        return getImageWithUserFillColor(iconName);
+    }
+    
+    /**
+     * Gets the icon image descriptor from the componnent's properties. If not found, return the default one.
+     */
+    @Override
+    public ImageDescriptor getImageDescriptor() {
+        String iconName = SpecializationPlugin.getIconName(instance, true);
+        
+        if ( iconName == null )
+            return super.getImageDescriptor();
+
+        return getImageDescriptorWithUserFillColor(iconName);
+    }
+}
