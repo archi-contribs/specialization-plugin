da9bda7e4bb14ae4cf17c68241390a2611f85bbc
Hervé <herve.jouin@gmail.com>
2017-08-24 21:34:57 +0200

    new

---
+++ sources/src/org/archicontribs/specialization/uiProvider/ValueUIProvider.java
@@ -0,0 +1,40 @@
+/**
+ * This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ */
+package org.archicontribs.specialization.uiProvider;
+
+import org.archicontribs.specialization.SpecializationPlugin;
+import org.archicontribs.specialization.figure.CollaborationFigure;
+import org.eclipse.gef.EditPart;
+import org.eclipse.swt.graphics.Image;
+
+import com.archimatetool.editor.diagram.editparts.ArchimateElementEditPart;
+
+
+/**
+ * Value UI Provider
+ * 
+ * @author Herve Jouin
+ */
+public class ValueUIProvider extends com.archimatetool.editor.ui.factory.elements.ValueUIProvider {
+    @Override
+    public EditPart createEditPart() {
+            // we override the standard method because we want our NodeFigure class to be called
+        return new ArchimateElementEditPart(CollaborationFigure.class);
+    }
+    
+    /**
+     * Gets the icon image from the component's properties. If not found, return the default one.
+     */
+    @Override
+    public Image getImage() {
+        String iconName = SpecializationPlugin.getIconName(instance, true);
+        
+        if ( iconName == null )
+            return super.getImage();
+
+        return getImageWithUserFillColor(iconName);
+    }
+}
