05ff2a1d418ea1cea2d65f204392d8812c8fa269
Hervé <herve.jouin@gmail.com>
2018-05-13 22:44:27 +0200

    removed unused import

--- sources/src/org/archicontribs/specialization/SpecializationVariable.java
+++ sources/src/org/archicontribs/specialization/SpecializationVariable.java
@@ -8,7 +8,6 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 
 import com.archimatetool.model.IArchimateDiagramModel;
