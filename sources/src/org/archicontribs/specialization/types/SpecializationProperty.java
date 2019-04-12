2b1e8c83a4716cdb50e6973965e4961b73222293
Hervé <herve.jouin@gmail.com>
2019-04-12 22:00:31 +0200

    rename file

--- sources/src/org/archicontribs/specialization/types/SpecializationProperty.java
+++ sources/src/org/archicontribs/specialization/types/SpecializationProperty.java
@@ -3,11 +3,11 @@ package org.archicontribs.specialization.types;
 import lombok.Getter;
 import lombok.Setter;
 
-public class Property {
+public class SpecializationProperty {
     @Getter @Setter private String name;
     @Getter @Setter private String value;
     
-    public Property(String name, String value) {
+    public SpecializationProperty(String name, String value) {
         this.name = name;
         this.value = value;
     }
