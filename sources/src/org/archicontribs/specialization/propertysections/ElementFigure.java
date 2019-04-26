4a323a8671033af59a85b57f815ef6634b0b5b81
Hervé <herve.jouin@gmail.com>
2019-04-26 18:11:19 +0200

    fix "can change icon" check

--- sources/src/org/archicontribs/specialization/propertysections/ElementFigure.java
+++ sources/src/org/archicontribs/specialization/propertysections/ElementFigure.java
@@ -34,6 +34,7 @@ public class ElementFigure extends Composite {
     static final SpecializationLogger logger = new SpecializationLogger(ElementFigure.class);
     
     static final int figureMargin = 2;
+    static final String canChangeIconString = "canChangeIcon";
     
     Label lblIconSize = null;
     Label lblIconLocation = null;
@@ -42,12 +43,14 @@ public class ElementFigure extends Composite {
     Text txtIconSize= null;
     Text txtIconLocation = null;
     
-    Composite compo1 = null;
-    Composite compo2 = null;
+    Composite outerCompo1 = null;
+    Composite innerCompo1 = null;
+    Composite outerCompo2 = null;
+    Composite innerCompo2 = null;
     Label figure1 = null;
     Label figure2 = null;
     
-    Label selectedFigure = null;
+    Composite selectedFigure = null;
 
     public ElementFigure(Composite parent, int type) {
         super(parent, type);
@@ -55,42 +58,69 @@ public class ElementFigure extends Composite {
         setLayout(new FormLayout());
         
         // figure 1
-        this.compo1 = new Composite(this, SWT.BORDER);
-        GridLayout gridLayout = new GridLayout();
-        gridLayout.marginWidth = figureMargin;
-        gridLayout.marginHeight = figureMargin;
-        this.compo1.setLayout(gridLayout);
-        this.compo1.addPaintListener(this.redrawListener);
+        this.outerCompo1 = new Composite(this, SWT.NONE);
+        this.setBackground(parent.getBackground());
         FormData fd = new FormData();
         fd.top = new FormAttachment(0, 0);
-        fd.width = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_WIDTH) + 2*figureMargin;
         fd.left = new FormAttachment(0, 0);
+        fd.width = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_WIDTH) + 2*figureMargin;
         fd.height = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_HEIGHT) + 2*figureMargin;
-        this.compo1.setLayoutData(fd);
+        this.outerCompo1.setLayoutData(fd);
+        this.outerCompo1.setLayout(new FormLayout());
+        //this.outerCompo1.addPaintListener(this.redrawListener);
+
+        this.innerCompo1 = new Composite(this.outerCompo1, SWT.NONE);
+        this.setBackground(parent.getBackground());
+        fd = new FormData();
+        fd.top = new FormAttachment(0, figureMargin);
+        fd.left = new FormAttachment(0, figureMargin);
+        fd.width = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_WIDTH);
+        fd.height = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_HEIGHT);
+        this.innerCompo1.setLayoutData(fd);
+        this.innerCompo1.setLayout(new FormLayout());
         
-        this.figure1 = new Label(compo1, SWT.NULL);
+        this.figure1 = new Label(this.innerCompo1, SWT.NULL);
         this.figure1.setLayoutData(new GridData(GridData.FILL_BOTH));
         this.figure1.setData("imageFigure", this);
         this.figure1.addMouseListener(this.selectListener);
-        
-        // figure 2
-        this.compo2 = new Composite(this, SWT.BORDER);
-        gridLayout = new GridLayout();
-        gridLayout.marginWidth = figureMargin;
-        gridLayout.marginHeight = figureMargin;
-        this.compo2.setLayout(gridLayout);
-        this.compo2.addPaintListener(this.redrawListener);
         fd = new FormData();
         fd.top = new FormAttachment(0, 0);
+        fd.left = new FormAttachment(0, 0);
+        fd.width = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_WIDTH);
+        fd.height = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_HEIGHT);
+        this.figure1.setLayoutData(fd);
+        
+        // figure 2
+        this.outerCompo2 = new Composite(this, SWT.NONE);
+        fd = new FormData();
+        fd.top = new FormAttachment(0, 0);
+        fd.left = new FormAttachment(this.outerCompo1, 5);
         fd.width = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_WIDTH) + 2*figureMargin;
-        fd.left = new FormAttachment(this.compo1, 5);
         fd.height = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_HEIGHT) + 2*figureMargin;
-        this.compo2.setLayoutData(fd);
+        this.outerCompo2.setLayoutData(fd);
+        this.outerCompo2.setLayout(new FormLayout());
+        //this.outerCompo2.addPaintListener(this.redrawListener);
+        
+        this.innerCompo2 = new Composite(this.outerCompo2, SWT.NONE);
+        this.setBackground(parent.getBackground());
+        fd = new FormData();
+        fd.top = new FormAttachment(0, figureMargin);
+        fd.left = new FormAttachment(0, figureMargin);
+        fd.width = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_WIDTH);
+        fd.height = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_HEIGHT);
+        this.innerCompo2.setLayoutData(fd);
+        this.innerCompo2.setLayout(new FormLayout());
 
-        this.figure2 = new Label(compo2, SWT.NULL);
+        this.figure2 = new Label(this.innerCompo2, SWT.NULL);
         this.figure2.setLayoutData(new GridData(GridData.FILL_BOTH));
         this.figure2.setData("imageFigure", this);
         this.figure2.addMouseListener(this.selectListener);
+        fd = new FormData();
+        fd.top = new FormAttachment(0, 0);
+        fd.left = new FormAttachment(0, 0);
+        fd.width = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_WIDTH);
+        fd.height = ArchiPlugin.INSTANCE.getPreferenceStore().getInt(com.archimatetool.editor.preferences.IPreferenceConstants.DEFAULT_ARCHIMATE_FIGURE_HEIGHT);
+        this.figure2.setLayoutData(fd);
 
         
         // buttons icon
@@ -98,34 +128,34 @@ public class ElementFigure extends Composite {
         this.btnNewIcon.setImage(SpecializationPlugin.NEW_ICON);
         this.btnNewIcon.setToolTipText("set Icon");
         this.btnNewIcon.setEnabled(false);
-        fd = new FormData();
-        fd.top = new FormAttachment(this.compo2, 0, SWT.TOP);
-        fd.left = new FormAttachment(this.compo2, 5);
-        fd.right = new FormAttachment(this.compo2, 40, SWT.RIGHT);
-        this.btnNewIcon.setLayoutData(fd);
         this.btnNewIcon.setVisible(false);
+        fd = new FormData();
+        fd.top = new FormAttachment(this.outerCompo2, 0, SWT.TOP);
+        fd.left = new FormAttachment(this.outerCompo2, 5);
+        fd.right = new FormAttachment(this.outerCompo2, 40, SWT.RIGHT);
+        this.btnNewIcon.setLayoutData(fd);
 
         this.btnDeleteIcon = new Button(this, SWT.PUSH);
         this.btnDeleteIcon.setImage(SpecializationPlugin.DELETE_ICON);
         this.btnDeleteIcon.setToolTipText("delete Icon");
         this.btnDeleteIcon.setEnabled(false);
+        this.btnDeleteIcon.setVisible(false);
         fd = new FormData();
         fd.top = new FormAttachment(this.btnNewIcon, 5);
-        fd.left = new FormAttachment(this.compo2, 5);
-        fd.right = new FormAttachment(this.compo2, 40, SWT.RIGHT);
+        fd.left = new FormAttachment(this.outerCompo2, 5);
+        fd.right = new FormAttachment(this.outerCompo2, 40, SWT.RIGHT);
         this.btnDeleteIcon.setLayoutData(fd);
-        this.btnDeleteIcon.setVisible(false);
 
         this.lblIconSize = new Label(this, SWT.NONE);
         this.lblIconSize.setForeground(this.getForeground());
         this.lblIconSize.setBackground(this.getBackground());
-        this.lblIconSize.setEnabled(false);
         this.lblIconSize.setText("Size:");
+        this.lblIconSize.setEnabled(false);
+        this.lblIconSize.setVisible(false);
         fd = new FormData();
-        fd.top = new FormAttachment(0, 8, SWT.TOP);
+        fd.top = new FormAttachment(0, 8);
         fd.left = new FormAttachment(this.btnNewIcon, 5);
         this.lblIconSize.setLayoutData(fd);
-        this.lblIconSize.setVisible(false);
 
         this.txtIconSize = new Text(this, SWT.BORDER);
         this.txtIconSize.setToolTipText("Size of the icon");
@@ -135,23 +165,23 @@ public class ElementFigure extends Composite {
         this.lblIconLocation = new Label(this, SWT.NONE);
         this.lblIconLocation.setForeground(this.getForeground());
         this.lblIconLocation.setBackground(this.getBackground());
-        this.lblIconLocation.setEnabled(false);
         this.lblIconLocation.setText("Location:");
+        this.lblIconLocation.setEnabled(false);
+        this.lblIconLocation.setVisible(false);
         fd = new FormData();
         fd.top = new FormAttachment(this.lblIconSize, 5);
         fd.left = new FormAttachment(this.lblIconSize, 0, SWT.LEFT);
         this.lblIconLocation.setLayoutData(fd);
-        this.lblIconLocation.setVisible(false);
 
         this.txtIconLocation = new Text(this, SWT.BORDER);
         this.txtIconLocation.setToolTipText("Location of the icon");
         this.txtIconLocation.setEnabled(false);
+        this.txtIconLocation.setVisible(false);
         fd = new FormData();
         fd.top = new FormAttachment(this.lblIconLocation, 0, SWT.CENTER);
         fd.left = new FormAttachment(this.lblIconLocation, 5);
         fd.right = new FormAttachment(this.lblIconLocation, 80, SWT.RIGHT);
         this.txtIconLocation.setLayoutData(fd);
-        this.txtIconLocation.setVisible(false);
 
         fd = new FormData();
         fd.top = new FormAttachment(this.lblIconSize, 0, SWT.CENTER);
@@ -176,44 +206,50 @@ public class ElementFigure extends Composite {
             IDiagramModelObjectFigure figure = ((ArchimateElementEditPart)ObjectUIFactory.INSTANCE.getProviderForClass(eClass).createEditPart()).getFigure();
 
             try {
+                // either the IDiagramModelObjectFigure delegates the drawing to a figure delegate,
+            	// and in that case, we need to check if the figure delegate allows to change the icon
                 Field field = figure.getClass().getSuperclass().getDeclaredField("fFigureDelegate1");
                 field.setAccessible(true);
-                this.figure1.setData("canchangeIcon", (field.get(figure) instanceof RectangleFigureDelegate));
+                this.outerCompo1.setData(canChangeIconString, (field.get(figure) instanceof RectangleFigureDelegate));
                 field.setAccessible(false);
                 
                 field = figure.getClass().getSuperclass().getDeclaredField("fFigureDelegate2");
                 field.setAccessible(true);
-                this.figure2.setData("canchangeIcon", (field.get(figure) instanceof RectangleFigureDelegate));
+                this.outerCompo2.setData(canChangeIconString, (field.get(figure) instanceof RectangleFigureDelegate));
                 field.setAccessible(false);
             } catch (@SuppressWarnings("unused") Exception err) {
-                logger.error("Failed to get the fFigureDelegate fields for "+figure);
+            	// either there is no figure delegate, and then the icon can be changed.
+            	this.outerCompo1.setData(canChangeIconString, true);
+            	this.outerCompo2.setData(canChangeIconString, true);
             }
             
         }
     }
     
-    void select(Label figure) {
+    void select(Composite figure) {
         this.selectedFigure = figure;
         
-        if ( (this.selectedFigure != null) && (this.selectedFigure.getData("canChangeIcon") != null) ) {
-            boolean canChangeIcon = (boolean)this.selectedFigure.getData("canChangeIcon");
+        this.outerCompo1.setBackground(figure == this.outerCompo1 ? ColorConstants.blue : this.outerCompo1.getParent().getBackground());
+        this.outerCompo2.setBackground(figure == this.outerCompo2 ? ColorConstants.blue : this.outerCompo2.getParent().getBackground());
+        
+        if ( (this.selectedFigure != null) && (this.selectedFigure.getData(canChangeIconString) != null) ) {
+            boolean canChangeIcon = (boolean)this.selectedFigure.getData(canChangeIconString);
             this.btnNewIcon.setVisible(canChangeIcon);
             this.btnDeleteIcon.setVisible(canChangeIcon);
             this.lblIconSize.setVisible(canChangeIcon);
             this.txtIconSize.setVisible(canChangeIcon);
             this.lblIconLocation.setVisible(canChangeIcon);
             this.txtIconLocation.setVisible(canChangeIcon);
+            
+            logger.trace("canchangeIcon = " + canChangeIcon);
         }
-
-        this.compo1.redraw();
-        this.compo2.redraw();
     }
 
     void select(int type) {
         if ( type == 0 )
-            select(this.figure1);
+            select(this.outerCompo1);
         else
-            select(this.figure2);
+            select(this.outerCompo2);
     }
     
     void setIconSize(String iconSize) {
@@ -224,23 +260,11 @@ public class ElementFigure extends Composite {
         this.txtIconLocation.setText(iconLocation);
     }
     
-    private PaintListener redrawListener = new PaintListener() {
-        @Override
-        public void paintControl(PaintEvent e) {
-            if ( e.widget == ElementFigure.this.selectedFigure ) {
-                GC graphics = e.gc;
-                graphics.setForeground(ColorConstants.blue);
-                graphics.setLineWidth(figureMargin);
-                Rectangle bounds = getBounds();
-                graphics.drawRectangle(1, 1, bounds.width - figureMargin, bounds.height - figureMargin);
-            }
-        }
-    };
-    
     private MouseAdapter selectListener = new MouseAdapter() {
         @Override
         public void mouseDown(MouseEvent e) {
-            select((Label)e.widget);
+            if ( ((Label)e.widget).getBackgroundImage() != null )
+            	select(((Label)e.widget).getParent().getParent());		// we select the outer composite
         }
     };
 }
