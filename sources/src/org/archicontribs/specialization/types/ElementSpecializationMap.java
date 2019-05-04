27e361ef93a18b6421fe7369a23e8929c131bd05
Hervé <herve.jouin@gmail.com>
2019-05-04 16:49:39 +0200

    add checks for null

--- sources/src/org/archicontribs/specialization/types/ElementSpecializationMap.java
+++ sources/src/org/archicontribs/specialization/types/ElementSpecializationMap.java
@@ -38,39 +38,46 @@ public class ElementSpecializationMap extends HashMap<String, List<ElementSpecia
     }
     
     public ElementSpecialization getElementSpecialization(String clazz, String name) {
-    	List<ElementSpecialization> types = get(clazz);
-    	if ( types != null ) {
-    		for ( int index = 0 ; index < types.size() ; ++index ) {
-    			if ( types.get(index).getSpecializationName().equals(name) )
-    				return types.get(index);
-    		}
+    	if ( (clazz != null) && !clazz.isEmpty() && (name != null) && !name.isEmpty() ) {
+	    	List<ElementSpecialization> types = get(clazz);
+	    	if ( types != null ) {
+	    		for ( int index = 0 ; index < types.size() ; ++index ) {
+	    			if ( types.get(index).getSpecializationName().equals(name) )
+	    				return types.get(index);
+	    		}
+	    	}
     	}
-    	
     	return null;
     }
     
     public void addElementSpecialization(String clazz, ElementSpecialization elementSpecialization) {
-    	List<ElementSpecialization> elementSpecializationList = get(clazz);
-    	if ( elementSpecializationList == null ) {
-    		elementSpecializationList = new ArrayList<ElementSpecialization>();
-    		put(clazz, elementSpecializationList);
+    	if ( (clazz != null) && !clazz.isEmpty() && (elementSpecialization != null) ) {
+	    	List<ElementSpecialization> elementSpecializationList = get(clazz);
+	    	if ( elementSpecializationList == null ) {
+	    		elementSpecializationList = new ArrayList<ElementSpecialization>();
+	    		put(clazz, elementSpecializationList);
+	    	}
+	    	elementSpecializationList.add(elementSpecialization);
     	}
-    	elementSpecializationList.add(elementSpecialization);
     }
     
     public void removeElementSpecialization(String clazz, ElementSpecialization elementSpecialization) {
-    	List<ElementSpecialization> elementSpecializationList = get(clazz);
-    	if ( elementSpecializationList != null ) {
-    		elementSpecializationList.remove(elementSpecialization);
+    	if ( (clazz != null) && !clazz.isEmpty() && (elementSpecialization != null) ) {
+	    	List<ElementSpecialization> elementSpecializationList = get(clazz);
+	    	if ( elementSpecializationList != null ) {
+	    		elementSpecializationList.remove(elementSpecialization);
+	    	}
     	}
     }
     
     public void removeElementSpecialization(String clazz, String elementName) {
-    	List<ElementSpecialization> elementSpecializationList = get(clazz);
-    	if ( elementSpecializationList != null ) {
-        	ElementSpecialization elementSpecialization = getElementSpecialization(clazz, elementName);
-        	if ( elementSpecialization != null )
-        		elementSpecializationList.remove(elementSpecialization);
+    	if ( (clazz != null) && !clazz.isEmpty() && (elementName != null) && !elementName.isEmpty() ) {
+	    	List<ElementSpecialization> elementSpecializationList = get(clazz);
+	    	if ( elementSpecializationList != null ) {
+	        	ElementSpecialization elementSpecialization = getElementSpecialization(clazz, elementName);
+	        	if ( elementSpecialization != null )
+	        		elementSpecializationList.remove(elementSpecialization);
+	    	}
     	}
     }
     
